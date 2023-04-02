62d3899b40713b4864c3fdf7a640860d447ecc76
ameen <afor1m3en@gmail.com>
2023-04-02 07:53:27 +0530

    add:recent-posts

--- dekhta/models.py
+++ dekhta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Poets(models.Model):
+    first_name = models.CharField(max_length = 25)
+    last_name = models.CharField(max_length = 25)
+    posts_count = models.IntegerField(default = 0)
+
+class Post(models.Model):
+    # user = models.CharField(Poets)
+    d_time = models.DateTimeField(default=datetime.now())
+    data = models.TextField()
+    poet_id = models.ForeignKey('Poets', on_delete=models.CASCADE, default=-1)
--- dekhta/views.py
+++ dekhta/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Poets, Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    all_posts = Post.objects.all().order_by('-id').values()
+    poets = Poets.objects.all().values()
+    return render(request, 'index.html',{'allposts':all_posts, 'poet':poets})
