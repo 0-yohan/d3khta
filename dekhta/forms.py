78058d10c5e2ea15a29863eff572b9da30159b88
ameen <afor1m3en@gmail.com>
2023-04-06 16:44:20 +0530

    logo

--- dekhta/forms.py
+++ dekhta/forms.py
@@ -6,7 +6,8 @@ class NewUserForm(UserCreationForm):
     first_name = forms.CharField(max_length = 25)
     last_name = forms.CharField(max_length = 25)
     email = forms.EmailField()
+    pfp = forms.ImageField(allow_empty_file = False)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'pfp','password1', 'password2']
